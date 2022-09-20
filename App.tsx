0ccf1258af8728d42e9079306f77efdc642ee31e
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-09-19 21:50:43 -0300

    feat: added routes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { Background } from './src/components/Background';
-import { Home } from './src/screens/Home';
 
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from "@expo-google-fonts/inter";
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -16,7 +16,7 @@ export default function App() {
 				backgroundColor="transparent"
 				translucent
 			/>
-			{fontsLoaded ? <Home /> : <Loading />}
+			{fontsLoaded ? <Routes /> : <Loading />}
 		</Background>
 	);
 }
